3272f8f100978bf25d98366b5083270824951bbf
Matylda <matylda.biela@icloud.com>
2025-10-22 15:42:19 +0100

    fixed merging

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const storyText = document.getElementById("story-text");
 
 const story = [
@@ -43,49 +42,3 @@ setTimeout(() => {
     window.location.href = "index.html";
   }
 }, 60000);
-=======
-const storyText = document.getElementById("story-text");
-
-const story = [
-  "Error 404. File not found.",
-  "here lies my creativity. 2006-2025. RIP.",
-  "-No need to be dramatic-",
-  "-How would you like to start this piece?-",
-  "I don't know.",
-  "-is there anything that interests you?-",
-  "I don't know",
-  "-What do you do in your spare time?-",
-  "Nothing.",
-  "-Don't you have any ideas to spark that creativity?-",
-  "-Anything at all?-",
-  "how do I create art when I feel like everything has been done before?",
-  "..."
-];
-
-let currentLine = 0;
-
-document.body.addEventListener("click", () => {
-  if (currentLine < story.length) {
-    fadeText(story[currentLine]);
-    currentLine++;
-  } else {
-    fadeText("...");
-  }
-});
-
-function fadeText(newText) {
-  storyText.style.opacity = 0;
-  setTimeout(() => {
-    storyText.innerText = newText;
-    storyText.style.opacity = 1;
-  }, 600);
-}
-
-// Automatically redirect to home page after 1 minute (60000 ms)
-setTimeout(() => {
-  // Only redirect if the user is NOT already on the homepage
-  if (!window.location.pathname.endsWith("index.html") && window.location.pathname !== "subpage") {
-    window.location.href = "index.html";
-  }
-}, 60000);
->>>>>>> 6022c1e4f3c92f0304270a94140aa5402dfbbdec
